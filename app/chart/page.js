259430421bc9e51ccbb1593b715b8a9fc44aa36b
ImpostorEngineer <posta.gereksiz@gmail.com>
2025-01-02 18:29:23 -0500

    fix theme mode assignment in Dashboard component

--- app/chart/page.js
+++ app/chart/page.js
@@ -31,7 +31,7 @@ export default function Dashboard({}) {
         categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
       },
       theme: {
-        mode: theme,
+        mode: theme === 'light' ? 'light' : 'dark',
         palette: 'palette6',
       },
     },
